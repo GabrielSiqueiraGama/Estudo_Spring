956223baa546bf9810f9ed8129d552dfd73bce39
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-05-02 12:25:48 -0300

    create: :tada: Criação do projeto

---
+++ src/test/java/br/com/zhant/estudo_spring/EstudoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zhant.estudo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
