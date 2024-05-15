9e8c6da71e34af5374375f740118d304934558d6
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-05-15 12:20:04 -0300

    feat: :seedling: Injeção de depedencia.

--- src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
+++ src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
@@ -1,19 +1,24 @@
 package br.com.zhant.estudo_spring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zhant.estudo_spring.models.entities.Product;
+import br.com.zhant.estudo_spring.models.repositories.ProductRepository;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@PostMapping
 	public Product newProduct(String name) {
 		Product product = new Product(name);
-		
+		productRepository.save(product);
 		return product;
 	}
 }
