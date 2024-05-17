2b9b2ff2aaebffe906aa6f7f1f2004c00d5419ce
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-05-16 23:21:14 -0300

    feat: :seedling: JPA test

--- src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
+++ src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
@@ -3,6 +3,8 @@ package br.com.zhant.estudo_spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zhant.estudo_spring.models.entities.Product;
@@ -16,8 +18,7 @@ public class ProductController {
 	private ProductRepository productRepository;
 	
 	@PostMapping
-	public Product newProduct(String name) {
-		Product product = new Product(name);
+	public @ResponseBody Product newProduct(Product product) {
 		productRepository.save(product);
 		return product;
 	}
--- src/main/java/br/com/zhant/estudo_spring/models/entities/Product.java
+++ src/main/java/br/com/zhant/estudo_spring/models/entities/Product.java
@@ -14,7 +14,17 @@ public class Product {
 	
 	private String name;
 
+	private double price;
+	
+	private double discount; 
+	
 	public Product() {
+	}
+	public Product(String name, double price, double discount) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.discount = discount;
 	}
 	public Product(String name) {
 		this.name = name;
@@ -22,17 +32,32 @@ public class Product {
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return name;
 	}
-
 	public void setNome(String name) {
 		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
 	}
 	
 	
