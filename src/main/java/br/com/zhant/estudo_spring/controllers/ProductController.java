7d168e90cb9fbbabff683e723eb0e00d33a4422a
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-05-19 23:17:55 -0300

    feat: :seedling: validation

--- src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
+++ src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
@@ -3,12 +3,12 @@ package br.com.zhant.estudo_spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zhant.estudo_spring.models.entities.Product;
 import br.com.zhant.estudo_spring.models.repositories.ProductRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/products")
@@ -18,7 +18,7 @@ public class ProductController {
 	private ProductRepository productRepository;
 	
 	@PostMapping
-	public @ResponseBody Product newProduct(Product product) {
+	public @ResponseBody Product newProduct(@Valid Product product) {
 		productRepository.save(product);
 		return product;
 	}
--- src/main/java/br/com/zhant/estudo_spring/models/entities/Product.java
+++ src/main/java/br/com/zhant/estudo_spring/models/entities/Product.java
@@ -4,18 +4,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
+
 public class Product {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@NotBlank
 	private String name;
 
+	@Min(0)
 	private double price;
-	
+	@Min(0)
+	@Max(1)
 	private double discount; 
 	
 	public Product() {
