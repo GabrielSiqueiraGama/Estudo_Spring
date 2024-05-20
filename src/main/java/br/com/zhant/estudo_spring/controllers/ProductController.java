04eb44cbf3dc1a38a675d5b0d80c322782c35549
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-05-20 12:45:20 -0300

    feat: :seedling: Crud system on products

--- src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
+++ src/main/java/br/com/zhant/estudo_spring/controllers/ProductController.java
@@ -1,8 +1,13 @@
 package br.com.zhant.estudo_spring.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +22,29 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	@PostMapping
-	public @ResponseBody Product newProduct(@Valid Product product) {
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Product saveProduct(@Valid Product product) {
 		productRepository.save(product);
 		return product;
+	}
+	
+	@GetMapping
+	public Iterable<Product> getProducts() {
+		return productRepository.findAll();
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Product> getProductsById(@PathVariable int id) {
+		return productRepository.findById(id);
+	}
+	
+//	@PutMapping
+//	public Product updateProduct(@Valid Product product) {
+//		productRepository.save(product);
+//		return product;
+//	}
+	@DeleteMapping(path = "/{id}")
+	public void deleteProduct(@PathVariable int id) {
+		productRepository.deleteById(id);
 	}
 }
